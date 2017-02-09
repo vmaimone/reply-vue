bd62d1f0b5d469c2813593c51ee9626bf818ed92
vmaimone <vmaimone@users.noreply.github.com>
2017-02-09 05:46:37 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const renderViewModel = require('./renderViewModel')
+
+function register(server, options, next) {
+  server.decorate('reply', 'vue', function ViewModel(data = {}) {
+    return renderViewModel(this.request, data)
+      .then(app => this.response(app))
+      .catch(({ code, data }) => {
+        this.response(data).code(code)
+      })
+  })
+  next()
+}
+
+exports.register = register
+
+exports.register.attributes = {
+  name: 'reply-vue',
+  version: '0.0.1'
+}
